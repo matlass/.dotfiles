844cf84ddef110b195b2b1a06a1f3406f320ace3
matlass_ <matthieu@thefau.com>
2025-08-23 17:33:11 +0200

    fixed je crois

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -29,10 +29,10 @@
       wl-clipboard
     ];
     plugins = with pkgs.vimPlugins; [
-      {
-        plugin = nvim-lspconfig;
-        config = toLuaFile ./nvim/plugin/lsp.lua;
-      }
+#      {
+#        plugin = nvim-lspconfig;
+#        config = toLuaFile ./nvim/plugin/lsp.lua;
+#      }
       {
         plugin = comment-nvim;
         config = toLua "require(\"Comment\").setup()";
@@ -61,7 +61,7 @@
       {
         plugin = (nvim-treesitter.withPlugins (p: [
           p.tree-sitter-nix
-	  p.tree-sitter-c
+	      p.tree-sitter-c
           p.tree-sitter-vim
           p.tree-sitter-bash
           p.tree-sitter-lua
