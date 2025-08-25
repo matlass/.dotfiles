eaffb5581e10fdcf0e8537f9a0c5238fed376b8f
matlass_ <matthieu@thefau.com>
2025-08-25 18:05:07 +0200

    jsp ce que j'ai changer

--- modules/nvim/options.lua
+++ modules/nvim/options.lua
@@ -1,10 +1,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = 'wl-clipboard'
 
 vim.o.number = true
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 vim.o.signcolumn = 'yes'
 
