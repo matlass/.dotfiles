410b32a00863a26ce53ccc0f4fa369de9cda594b
matlass_ <matthieu@thefau.com>
2025-08-30 13:06:37 +0200

    end config

---
+++ modules/scripts/save-space.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+set -e
+echo "starting to save space"
+sudo nix-env --delete-generations old --profile /nix/var/nix/profiles/system
+home-manager expire-generations '-1 days'
+
+echo "removing garbage"
+sudo nix-collect-garbage -d
+nix-collect-garbage -d
+nix-store --gc
+nix-store --optimise
