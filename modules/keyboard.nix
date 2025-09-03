4bb5e1450a0007ddc0524a5698ba959cc6aff962
matlass_ <matthieu@thefau.com>
2025-09-04 00:31:45 +0200

    smlfqjdmfk home row

--- modules/keyboard.nix
+++ modules/keyboard.nix
@@ -16,7 +16,6 @@ in
       input  (device-file "/dev/input/by-path/platform-i8042-serio-0-event-kbd")
       output (uinput-sink "kmonad")
       fallthrough true
-      allow-capslock true
     )
 
     (defsrc
