500bb1932116d98c22f536d269bba3276891bec0
matlass_ <matthieu@thefau.com>
2025-09-04 00:53:23 +0200

    il a rien changer

--- modules/keyboard.nix
+++ modules/keyboard.nix
@@ -29,16 +29,7 @@ in
 (deflayer home-row
   grave 1 2 3 4 5 6 7 8 9 0 minus equal
   tab q w e r t y u i o p leftbrace rightbrace
-  (tap-hold-next-release a lalt)
-  (tap-hold-next-release s lmeta)
-  (tap-hold-next-release d lctl)
-  (tap-hold-next-release f lshift)
-  g h
-  (tap-hold-next-release j lalt)
-  (tap-hold-next-release k lmeta)
-  (tap-hold-next-release l lctl)
-  (tap-hold-next-release semicolon lshift)
-  apostrophe backslash
+  (tap-hold-next-release a lalt) (tap-hold-next-release s lmeta) (tap-hold-next-release d lctl) (tap-hold-next-release f lshift) g h (tap-hold-next-release j lalt) (tap-hold-next-release k lmeta) (tap-hold-next-release l lctl) (tap-hold-next-release semicolon lshift) apostrophe backslash
   leftshift 102nd z x c v b n m comma dot slash rightshift
   space
 )
