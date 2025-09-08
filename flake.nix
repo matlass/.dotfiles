9a0241176fce8b0b0db053f267864ef9bfa5309c
matlass_ <matthieu@thefau.com>
2025-09-08 23:05:44 +0200

    change home-manager branche

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     stable.url = "github:nixos/nixpkgs/nixos-25.05";
-    home-manager.url = "github:nix-community/home-manager/";
+    home-manager = {
+      url = "github:nix-community/home-manager/";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nvf.url = "github:notashelf/nvf";
   };
 
