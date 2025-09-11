db41f74497fd9f91a52af614bb019b8460fecd8a
matlass_ <matthieu@thefau.com>
2025-09-11 22:44:22 +0200

    neovim config

--- modules/applications/neovim/navigation.nix
+++ modules/applications/neovim/navigation.nix
@@ -45,8 +45,8 @@
           window = {
             border = "rounded";
             position = "bottom";
-            margin = { 1, 0, 1, 0 };
-            padding = { 2, 2, 2, 2 };
+            margin = [ 1 0 1 0 ];
+            padding = [ 2 2 2 2 ];
             winblend = 0;
           };
           layout = {
