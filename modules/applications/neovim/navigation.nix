5136811d39319859f885c47f4030fe43d45c3100
matlass_ <matthieu@thefau.com>
2025-09-11 22:47:12 +0200

    nixos config

--- modules/applications/neovim/navigation.nix
+++ modules/applications/neovim/navigation.nix
@@ -50,8 +50,8 @@
             winblend = 0;
           };
           layout = {
-            height = { min = 4; max = 25 };
-            width = { min = 20; max = 50 };
+            height = { min = 4; max = 25; };
+            width = { min = 20; max = 50; };
             spacing = 3;
             align = "left";
           };
