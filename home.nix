eb99bd520e9cddeb0fa5e0df869afd662b488cb4
matlass_ <matthieu@thefau.com>
2025-09-21 18:44:52 +0200

    spotify

--- home.nix
+++ home.nix
@@ -17,6 +17,7 @@
   home.homeDirectory = "/home/matthieu";
   home.stateVersion = "25.05";
 
+  nixpkgs.config.allowUnfree = true;
   home.sessionVariables = {
     EDITOR = "nvim";
     NIXOS_OZONE_WL = "1";
