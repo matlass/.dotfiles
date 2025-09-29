1904b09df72f5b79da205a35b3e423d88a7ab045
matlass_ <matthieu@thefau.com>
2025-09-29 19:33:02 +0200

    test 2

--- modules/desktop/hyprpaper.nix
+++ modules/desktop/hyprpaper.nix
@@ -12,10 +12,12 @@
       splash = false;
       splash_offset = 2.0;
       preload = [
-        ",${paths.nixDarkImage}"
+        "${paths.nixDarkImage}"
+        "/home/matthieu/Downloads/nix-d-nord-purple.jpg"
       ];
       wallpaper = [
-        ",${paths.nixDarkImage}"
+        # ",${paths.nixDarkImage}"
+        ",/home/matthieu/Downloads/nix-d-nord-purple.jpg"
       ];
     };
   };
