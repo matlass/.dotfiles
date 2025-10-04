276603d154452b6617f1bc08f6254f2d9ff3a0ab
matlass_ <matthieu@thefau.com>
2025-10-04 23:19:22 +0200

    fuck

--- hosts/bok/kanata.nix
+++ hosts/bok/kanata.nix
@@ -11,33 +11,33 @@
     KERNEL=="uinput", MODE="0666", OPTIONS+="static_node=uinput"
   '';
 
-  services.kanata = {
-    enable = true;
-    keyboards = {
-      laptop = {
-        devices = [
-          "/dev/input/event0"
-        ];
-        extraDefCfg = "process-unmapped-keys yes";
-        config = ''
-          (defsrc
-            a s d f j k l ;
-          )
-          (defalias
-            a (tap-hold 200 200 a lctl)
-            s (tap-hold 200 200 s lalt)
-            d (tap-hold 200 200 d lsft)
-            f (tap-hold 200 200 f lmet)
-            j (tap-hold 200 200 j rmet)
-            k (tap-hold 200 200 k rsft)
-            l (tap-hold 200 200 l ralt)
-            ; (tap-hold 200 200 ; rctl)
-          )
-          (deflayer base
-            @a @s @d @f @j @k @l @;
-          )
-        '';
-      };
-    };
-  };
+  # services.kanata = {
+  #   enable = true;
+  #   keyboards = {
+  #     laptop = {
+  #       devices = [
+  #         "/dev/input/event0"
+  #       ];
+  #       extraDefCfg = "process-unmapped-keys yes";
+  #       config = ''
+  #         (defsrc
+  #           a s d f j k l ;
+  #         )
+  #         (defalias
+  #           a (tap-hold 200 200 a lctl)
+  #           s (tap-hold 200 200 s lalt)
+  #           d (tap-hold 200 200 d lsft)
+  #           f (tap-hold 200 200 f lmet)
+  #           j (tap-hold 200 200 j rmet)
+  #           k (tap-hold 200 200 k rsft)
+  #           l (tap-hold 200 200 l ralt)
+  #           ; (tap-hold 200 200 ; rctl)
+  #         )
+  #         (deflayer base
+  #           @a @s @d @f @j @k @l @;
+  #         )
+  #       '';
+  #     };
+  #   };
+  # };
 }
