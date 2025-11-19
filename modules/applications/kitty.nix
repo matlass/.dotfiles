8630b4ed512af04af02f2f9fb8bca7940b7b1060
matlass_ <matthieu@thefau.com>
2025-11-19 11:34:26 +0100

    fuck cursor tary

--- modules/applications/kitty.nix
+++ modules/applications/kitty.nix
@@ -1,5 +1,8 @@
-{config, pkgs, ... }:
 {
+  config,
+  pkgs,
+  ...
+}: {
   programs.kitty = {
     enable = true;
     font = {
@@ -8,8 +11,6 @@
     };
     themeFile = "Catppuccin-Mocha";
     settings = {
-      cursor_trail = "1";
-      cursor_trail_decay = "0.2 0.8";
       confirm_os_window_close = "0";
       background_opacity = "0.7";
     };
