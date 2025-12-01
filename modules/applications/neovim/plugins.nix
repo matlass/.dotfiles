148159592941b108bd0b70ea0646d95388d60c44
matlass_ <matthieu@thefau.com>
2025-12-01 21:21:22 +0100

    fixed

--- modules/applications/neovim/plugins.nix
+++ modules/applications/neovim/plugins.nix
@@ -1,23 +1,29 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   programs.nvf.settings.vim = {
     lazy.plugins = {
       # Enhanced comments with treesitter
+      # "vimplugin-ts-comments.nvim" = {
+      #   package = pkgs.vimUtils.buildVimPlugin {
+      #     name = "ts-comments.nvim";
+      #     src = pkgs.fetchFromGitHub {
+      #       owner = "folke";
+      #       repo = "ts-comments.nvim";
+      #       rev = "main";
+      #       sha256 = "7uCJDt/w4q+Huoeh7wTjxicVp6+XWt8LHOXkgC5INDU=";
+      #     };
+      #   };
+      #   after = ''require("ts-comments")'';
+      #   event = ["BufReadPost" "BufNewFile"];
+      # };
+      #
       "vimplugin-ts-comments.nvim" = {
-        package = pkgs.vimUtils.buildVimPlugin {
-          name = "ts-comments.nvim";
-          src = pkgs.fetchFromGitHub {
-            owner = "folke";
-            repo = "ts-comments.nvim";
-            rev = "main";
-            sha256 = "7uCJDt/w4q+Huoeh7wTjxicVp6+XWt8LHOXkgC5INDU=";
-          };
-        };
+        package = pkgs.vimUtils.buildVimPlugin;
+        setupModule = "ts-comments";
+        cmd = ["ts-comments"];
         after = ''require("ts-comments")'';
         event = ["BufReadPost" "BufNewFile"];
       };
-      
+
       # Advanced search and replace
       "grug-far.nvim" = {
         package = pkgs.vimPlugins.grug-far-nvim;
