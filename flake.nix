737d6646e433f1d2fd98d8d3591b3c21191905c4
matlass_ <matthieu@thefau.com>
2025-12-04 17:13:26 +0100

    ptn de space

--- flake.nix
+++ flake.nix
@@ -56,9 +56,9 @@
         ];
       runScript = "bash";
       profile = ''
-        export SDL2_INCLUDE_PATH="${pkgs.SDL2. dev}/include/SDL2"
-        export C_INCLUDE_PATH="${pkgs.SDL2. dev}/include:${pkgs. sdl2_image.dev}/include"
-        export LIBRARY_PATH="${pkgs.SDL2}/lib:${pkgs.sdl2_image}/lib"
+        export SDL2_INCLUDE_PATH="${pkgs.SDL2.dev}/include/SDL2"
+        export C_INCLUDE_PATH="${pkgs.SDL2.dev}/include:${pkgs.SDL2_image.dev}/include"
+        export LIBRARY_PATH="${pkgs.SDL2}/lib:${pkgs.SDL2_image}/lib"
       '';
     };
     devShells.${system}.default = self.packages.${system}.sdl-fhs. env;
