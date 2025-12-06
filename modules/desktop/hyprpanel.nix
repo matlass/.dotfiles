84f879d76dc6980852e52561a9ec829a7c0431bd
matlass_ <matthieu@thefau.com>
2025-12-06 19:15:50 +0100

    custom hyprpanel

--- modules/desktop/hyprpanel.nix
+++ modules/desktop/hyprpanel.nix
@@ -1,5 +1,35 @@
 {...}: {
   programs.hyprpanel = {
     enable = true;
+    settings = {
+      layout = {
+        bar.layouts = {
+          "0" = {
+            left = ["dashboard" "workspaces" "windowtitle"];
+            middle = ["media" "clock"];
+            right = ["volume" "network" "bluetooth" "battery" "notifications"];
+          };
+        };
+      };
+      bar.launcher.autoDetectIcon = true;
+      bar.workspaces.show_icons = true;
+
+      menus = {
+        dashboard = {
+          directories.enabled = false;
+          stats.enable_gpu = true;
+        };
+        clock = {
+          time = {
+            military = true;
+            hideSeconds = true;
+          };
+        };
+      };
+      theme.font = {
+        name = "CaskaydiaCove NF";
+        size = "16px";
+      };
+    };
   };
 }
