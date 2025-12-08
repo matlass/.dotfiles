d63e3b7cf4c51e4687884f479211605748d894d9
matlass_ <matthieu@thefau.com>
2025-12-08 15:58:12 +0100

    end test

--- modules/desktop/hyprpanel.nix
+++ modules/desktop/hyprpanel.nix
@@ -3,54 +3,54 @@
     hyprpanel
   ];
 
-  # programs.hyprpanel = {
-  #   enable = true;
-  #   settings = {
-  #     layout = {
-  #       bar.layouts = {
-  #         "0" = {
-  #           left = [
-  #             "dashboard"
-  #             "workspaces"
-  #             "windowtitle"
-  #           ];
-  #           middle = [
-  #             "media"
-  #             "clock"
-  #           ];
-  #           right = [
-  #             "volume"
-  #             "network"
-  #             "bluetooth"
-  #             "battery"
-  #             "notifications"
-  #           ];
-  #         };
-  #       };
-  #     };
-  #     bar = {
-  #       launcher.autoDetectIcon = true;
-  #       workspaces.show_icons = true;
-  #     };
-  #
-  #     menus.dashboard = {
-  #       directories.enabled = false;
-  #       stats.enable_gpu = true;
-  #     };
-  #     menus.clock = {
-  #       time = {
-  #         military = true;
-  #         hideSeconds = true;
-  #       };
-  #       weather.unit = "metric";
-  #     };
-  #     theme = {
-  #       font = {
-  #         name = "CaskaydiaCove NF";
-  #         size = "14px";
-  #       };
-  #       bar.transparent = true;
-  #     };
-  #   };
-  # };
+  programs.hyprpanel = {
+    enable = true;
+    settings = {
+      layout = {
+        bar.layouts = {
+          "0" = {
+            left = [
+              "dashboard"
+              "workspaces"
+              "windowtitle"
+            ];
+            middle = [
+              "media"
+              "clock"
+            ];
+            right = [
+              "volume"
+              "network"
+              "bluetooth"
+              "battery"
+              "notifications"
+            ];
+          };
+        };
+      };
+      bar = {
+        launcher.autoDetectIcon = true;
+        workspaces.show_icons = true;
+      };
+
+      menus.dashboard = {
+        directories.enabled = false;
+        stats.enable_gpu = true;
+      };
+      menus.clock = {
+        time = {
+          military = true;
+          hideSeconds = true;
+        };
+        weather.unit = "metric";
+      };
+      theme = {
+        font = {
+          name = "CaskaydiaCove NF";
+          size = "14px";
+        };
+        bar.transparent = true;
+      };
+    };
+  };
 }
