f3a16760192963bf7ef9010ab47cf54796ee2c36
matlass_ <matthieu@thefau.com>
2025-12-08 15:57:06 +0100

    ts

--- modules/desktop/hyprpanel.nix
+++ modules/desktop/hyprpanel.nix
@@ -1,52 +1,56 @@
-{...}: {
-  programs.hyprpanel = {
-    enable = true;
-    # settings = {
-    #   layout = {
-    #     bar.layouts = {
-    #       "0" = {
-    #         left = [
-    #           "dashboard"
-    #           "workspaces"
-    #           "windowtitle"
-    #         ];
-    #         middle = [
-    #           "media"
-    #           "clock"
-    #         ];
-    #         right = [
-    #           "volume"
-    #           "network"
-    #           "bluetooth"
-    #           "battery"
-    #           "notifications"
-    #         ];
-    #       };
-    #     };
-    #   };
-    #   bar = {
-    #     launcher.autoDetectIcon = true;
-    #     workspaces.show_icons = true;
-    #   };
-    #
-    #   menus.dashboard = {
-    #     directories.enabled = false;
-    #     stats.enable_gpu = true;
-    #   };
-    #   menus.clock = {
-    #     time = {
-    #       military = true;
-    #       hideSeconds = true;
-    #     };
-    #     weather.unit = "metric";
-    #   };
-    #   theme = {
-    #     font = {
-    #       name = "CaskaydiaCove NF";
-    #       size = "14px";
-    #     };
-    #     bar.transparent = true;
-    #   };
-    # };
-  };
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    hyprpanel
+  ];
+
+  # programs.hyprpanel = {
+  #   enable = true;
+  #   settings = {
+  #     layout = {
+  #       bar.layouts = {
+  #         "0" = {
+  #           left = [
+  #             "dashboard"
+  #             "workspaces"
+  #             "windowtitle"
+  #           ];
+  #           middle = [
+  #             "media"
+  #             "clock"
+  #           ];
+  #           right = [
+  #             "volume"
+  #             "network"
+  #             "bluetooth"
+  #             "battery"
+  #             "notifications"
+  #           ];
+  #         };
+  #       };
+  #     };
+  #     bar = {
+  #       launcher.autoDetectIcon = true;
+  #       workspaces.show_icons = true;
+  #     };
+  #
+  #     menus.dashboard = {
+  #       directories.enabled = false;
+  #       stats.enable_gpu = true;
+  #     };
+  #     menus.clock = {
+  #       time = {
+  #         military = true;
+  #         hideSeconds = true;
+  #       };
+  #       weather.unit = "metric";
+  #     };
+  #     theme = {
+  #       font = {
+  #         name = "CaskaydiaCove NF";
+  #         size = "14px";
+  #       };
+  #       bar.transparent = true;
+  #     };
+  #   };
+  # };
 }
