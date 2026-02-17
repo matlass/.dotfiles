2e3645982c03e9b63bd2bbfb4795481f24ccfb60
matlass_ <matthieu@thefau.com>
2026-02-17 13:45:42 +0100

    change save_space

--- modules/scripts/save-space.sh
+++ modules/scripts/save-space.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 set -e
 echo "starting to save space"
+echo
+echo
+echo
 sudo nix-env --delete-generations old --profile /nix/var/nix/profiles/system
 home-manager expire-generations '-1 days'
-
+echo
+echo
+echo
 echo "removing garbage"
+echo
+echo
+echo
 sudo nix-collect-garbage -d
 nix-collect-garbage -d
 nix-store --gc
